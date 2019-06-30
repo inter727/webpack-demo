7f9c72172d7a5c4be7e5e7b3b24e7e0c3df883c7
inter727 <inter727@163.com>
2019-06-30 19:43:56 +0800

    loader的使用

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hello,world');
+import avatar from './avatar.jpg'
+import './index.css'
+
+var root = document.getElementById('root')
+var img = new Image()
+img.src = avatar
+img.classList.add('avatar')
+
+root.appendChild(img)
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,24 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
-  mode: 'production'
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name]_[hash].[ext]'
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']   //从右到左的顺序调用，所以顺序不能错
+      }
+    ]
+  },
+  plugins: [require('autoprefixer')]
 }
 
