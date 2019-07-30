adcb6162470c0cda274ac02c9fcc760fc89c19b2
inter727 <inter727@163.com>
2019-07-30 17:53:21 +0800

    代码分离

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-import '@babel/polyfill';
+// import _ from 'lodash'
+import '@babel/polyfill'
 import './style.css'
 import { add } from './main'
 
 add(1, 4)
+// console.log(_.join(['Dell','Lee'], ' '))
+
+document.addEventListener('click', () => {
+  getComponent().then(ele => {
+    document.getElementById('root').append(ele)
+  })
+})
+
+function getComponent() {
+  //括号内魔法注释，含义是告诉 Webpack 异步模块的名字为 lodash
+  return import(/* webpackChunkName: 'lodash' */'lodash').then(({default: _}) => {
+    let div = document.createElement('div')
+    div.innerHTML = _.join([1, 2], 3)
+    return div
+  })
+}
