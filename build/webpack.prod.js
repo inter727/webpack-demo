2d5efd980053954b71eeefae8e32f2a79993b021
inter727 <inter727@163.com>
2019-08-03 14:48:31 +0800

    用占位符解决浏览器缓存问题

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,9 +1,19 @@
 const merge = require('webpack-merge')
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
 const commonConfig = require('./webpack.common')
 
 const prodConfig = {
+  output: {
+    filename: '[name].[contenthash].js',
+    chunkFilename: '[name].[contenthash].js'
+  },
   mode: 'production',
   devtool: 'cheap-module-source-map',
+  plugins: [
+    new miniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    })
+  ],
   optimization: {
     usedExports: true
   }
